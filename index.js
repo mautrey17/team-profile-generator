cfe69321e95ee42828283c61ea7876d426804583
Matt Autrey <mautrey17@gmail.com>
2021-02-09 22:53:05 -0500

    import classes to index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
 //list of employees
 const employeeList = [];
