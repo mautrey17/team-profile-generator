c707678c92498a697c3a293daa859190b9288e95
Matt Autrey <mautrey17@gmail.com>
2021-02-16 21:52:47 -0500

    updated employee.test

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,31 +1,27 @@
 const Employee = require("../lib/employee");
 
-test('can create new instance of employee', () => {
-    const e = new Employee();
+describe('Employee', () => {
+    it('can create new instance of employee', () => {
+        const e = new Employee();
 
-    expect(typeof(e)).toBe('object')
-});
+        expect(typeof(e)).toBe('object')
+    });
 
-test('can get name using getName()', () => {
-    const e = new Employee('Matt');
+    it('can get name using getName()', () => {
+        const e = new Employee('Matt');
 
-    expect(e.getName()).toBe('Matt');
-});
+        expect(e.getName()).toBe('Matt');
+    });
 
-test('can get id using getId()', () => {
-    const e = new Employee('Matt', 2);
+    it('can get id using getId()', () => {
+        const e = new Employee('Matt', 2);
+    
+        expect(e.getId()).toBe(2);
+    });
 
-    expect(e.getId()).toBe(2);
-});
-
-test('can get email using getEmail()', () => {
-    const e = new Employee('Matt', 2, 'test.mail.com');
-
-    expect(e.getEmail()).toEqual('test.mail.com');
-});
-
-test('can get role using getRole()', () => {
-    const e = new Employee('Matt', 2, 'test.mail.com');
-
-    expect(e.getRole()).toEqual('Employee');
-});
+    it('can get role using getRole()', () => {
+        const e = new Employee('Matt', 2, 'test.mail.com');
+    
+        expect(e.getRole()).toBe('Employee');
+    });
+})
